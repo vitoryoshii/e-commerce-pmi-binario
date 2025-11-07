ed1a16e98e464cfd6f254323f8fdcf3fd876b241
vitor.yoshii <vitoryoshiiprofissional@gmail.com>
2025-11-07 17:06:00 -0300

    refactor: Organizado o código sem alterar a lógica

--- db/setup_database.py
+++ db/setup_database.py
@@ -2,16 +2,16 @@ import sqlite3
 import random
 import time
 
-# ---- PRODUTOS PARA POPULAÇÃO DO BANCO - ECOMMERCE TECH ----
-nomes_base = ["Smartphone", "Notebook", "Teclado", "Mouse", "Monitor", "Cadeira Gamer", "Fone de Ouvido"]
-marcas = ["Tech", "Hyper", "Quantum", "Future", "Eco", "Stellar"]
+# ==== PRODUTOS PARA POPULAÇÃO DO BANCO - ECOMMERCE TECH ====
+NOMES_PRODUTOS = ["Smartphone", "Notebook", "Teclado", "Mouse", "Monitor", "Cadeira Gamer", "Fone de Ouvido"]
+MARCAS = ["Tech", "Hyper", "Quantum", "Future", "Eco", "Stellar"]
 
 DB_FILE = "db/ecommerce.db"
 
 
-# ------------------------------------------------------
+# ===========================================================
 # FUNÇÃO DE POPULAÇÃO: popular_banco
-# ------------------------------------------------------
+# ===========================================================
 def popular_banco():
     """
     Cria e popula o banco de dados 'ecommerce.db' com produtos fictícios.
@@ -53,7 +53,7 @@ def popular_banco():
 
     ids_gerados = random.sample(range(10000000, 20000000), num_produtos)
     for product_id in ids_gerados:
-        nome = f"{random.choice(nomes_base)} {random.choice(marcas)} X{random.randint(100, 999)}"
+        nome = f"{random.choice(NOMES_PRODUTOS)} {random.choice(MARCAS)} X{random.randint(100, 999)}"
         preco = round(random.uniform(50.0, 5000.0), 2)
         cursor.execute("INSERT INTO produtos (id_produto, nome_produto, preco) VALUES (?, ?, ?)", (product_id, nome, preco))
 
@@ -65,8 +65,8 @@ def popular_banco():
     print(f"Banco de dados populado com sucesso em {end_time - start_time:.2f} segundos!")
 
 
-# ------------------------------------------------------
+# ===========================================================
 # CHAMADA DE SISTEMA PRINCIPAL - POPULAÇÃO DO BANCO
-# ------------------------------------------------------
+# ===========================================================
 if __name__ == '__main__':
     popular_banco()
